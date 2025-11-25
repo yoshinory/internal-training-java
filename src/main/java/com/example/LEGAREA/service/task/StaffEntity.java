208a96f17c6de1b79c53084a15111dcac80c1bfc
T_COMPUTER\kenta <kentarou010300@gmail.com>
2025-11-25 14:27:48 +0900

    LEGAREA研修作成

---
+++ src/main/java/com/example/LEGAREA/service/task/StaffEntity.java
@@ -0,0 +1,12 @@
+package com.example.LEGAREA.service.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StaffEntity {
+    private String staffid;
+    private String name;
+    private String division;
+}
